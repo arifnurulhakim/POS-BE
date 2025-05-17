f277859d845bf4aef22f193a97bdc4f23da1625b
arifnurulhakim <arifnhakim23@gmail.com>
2025-05-17 20:21:51 +0700

    dd

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import app from "./app";
-import { connectMongo } from "./config/mongo";
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    await connectMongo();
+    // Nonaktifkan MongoDB connection
+    // await connectMongo();
+
     app.listen(PORT, () => {
       console.log(`🚀 Server ready at http://localhost:${PORT}`);
     });
   } catch (err) {
-    console.error("❌ MongoDB connection failed:", err);
+    console.error("❌ Error starting server:", err);
     process.exit(1);
   }
 };
