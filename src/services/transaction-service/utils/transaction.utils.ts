6e99972e70369e79d355ff28eeea45472e30e375
arifnurulhakim <arifnhakim23@gmail.com>
2025-05-19 10:26:41 +0700

    sdd

--- src/services/transaction-service/utils/transaction.utils.ts
+++ src/services/transaction-service/utils/transaction.utils.ts
@@ -46,15 +46,15 @@ export const reformTransaction = (transaction: any): ReformattedTransaction => {
     status: transaction.status,
     customer_name: transaction.customer_name,
     customer_email: transaction.customer_email,
-    snap_token: transaction.snap_token,
-    snap_redirect_url: transaction.snap_redirect_url,
-    payment_method: transaction.payment_method,
-    products: transaction.transactions_items.map((transactionItem: any) => ({
-      id: transactionItem.product_id,
-      name: transactionItem.product_name,
-      price: transactionItem.price,
-      quantity: transactionItem.quantity,
-      image: transactionItem.products?.image || null,
+    snap_token: transaction.snap_token ?? null,
+    snap_redirect_url: transaction.snap_redirect_url ?? null,
+    payment_method: transaction.payment_method ?? null,
+    products: (transaction.transactions_items ?? []).map((transactionItem: any) => ({
+      id: transactionItem.product?.id ?? '',
+      name: transactionItem.product?.name ?? '',
+      price: transactionItem.product?.price ?? 0,
+      quantity: transactionItem.quantity ?? 0,
+      image: transactionItem.product?.image_url ?? null,
     })),
   };
 };
